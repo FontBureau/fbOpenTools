f7c19c1a43143f8205ff212dbaf64109a5034129
David Jonathan Ross <djr@fontbureau.com>
2012-05-26 01:54:46 -0400

    First draft of GlyphSelect and OverlayUFOs

---
+++ GlyphSelect/Glyph Select.roboFontExt/lib/GlyphSelect.py
@@ -0,0 +1,404 @@
+# -*- coding: UTF-8 -*-  
+#
+# ----------------------------------------------------------------------------------
+from vanilla import *
+from AppKit import *
+from defconAppKit.windows.baseWindow import BaseWindowController
+from fnmatch import fnmatch
+
+#############
+# RESOURCES #
+#############
+
+def splitName(name):
+    """Splits a glyph name into a (baseName string, suffix string) tuple."""
+    baseName, suffix = '', ''
+    nameElements = name.split('.')
+    if len(nameElements) > 0:
+        baseName = nameElements[0]
+        if len(nameElements) > 1:
+            suffix = '.'.join(nameElements[1:])
+        else:
+            suffix = ''
+    return baseName, suffix
+
+UNICODE_CATEGORY_ORDER = ['Lu', 'Ll', 'Lt', 'Lm', 'Lo', 'Mn', 'Mc', 'Me', 'Nd', 'Nl', 'No', 'Pc', 'Pd', 'Ps', 'Pe', 'Pi', 'Pf', 'Po', 'Sm', 'Sc', 'Sk', 'So', 'Zs', 'Zl', 'Zp', 'Cc', 'Cf', 'Cs', 'Co', 'Cn']
+UNICODE_CATEGORIES = {
+    'Lu': 'Letter, Uppercase',                #an uppercase letter
+    'Ll': 'Letter, Lowercase',                #a lowercase letter
+    'Lt': 'Letter, Titlecase',                #a digraphic character, with first part uppercase
+    'Lm': 'Letter, Modifier',                #a modifier letter
+    'Lo': 'Letter, Other',                    #other letters, including syllables and ideographs
+    'Mn': 'Mark, Nonspacing',                #a nonspacing combining mark (zero advance width)
+    'Mc': 'Mark, Spacing',                    #a spacing combining mark (positive advance width)
+    'Me': 'Mark, Enclosing',                #an enclosing combining mark
+    'Nd': 'Number, Decimal Digit',            #a decimal digit
+    'Nl': 'Number, Letter',                    #a letterlike numeric character
+    'No': 'Other Number',                    #a numeric character of other type
+    'Pc': 'Punctuation, Connector',            #a connecting punctuation mark, like a tie
+    'Pd': 'Punctuation, Dash',                #a dash or hyphen punctuation mark
+    'Ps': 'Punctuation, Open',                #an opening punctuation mark (of a pair)
+    'Pe': 'Punctuation, Close',                #a closing punctuation mark (of a pair)
+    'Pi': 'Punctuation, Initial',            #an initial quotation mark
+    'Pf': 'Punctuation, Final',                #a final quotation mark
+    'Po': 'Punctuation, Other',                #a punctuation mark of other type
+    'Sm': 'Symbol, Math',                    #a symbol of primarily mathematical use
+    'Sc': 'Symbol, Currency',                #a currency sign
+    'Sk': 'Symbol, Modifier',                #a non-letterlike modifier symbol
+    'So': 'Symbol, Other',                    #a symbol of other type
+    'Zs': 'Separator, Space',                #a space character (of various non-zero widths)
+    'Zl': 'Separator, Line',                #U+2028 LINE SEPARATOR only
+    'Zp': 'Separator, Paragraph',            #U+2029 PARAGRAPH SEPARATOR only
+    'Cc': 'Other, Control',                    #a C0 or C1 control code
+    'Cf': 'Other, Format',                    #a format control character
+    'Cs': 'Other, Surrogate',                #a surrogate code point
+    'Co': 'Other, Private Use',                #a private-use character
+    'Cn': 'Other, Unassigned',                #a reserved unassigned code point or a noncharacter
+}
+UNICODE_CATEGORY_ORDER_COMBINED = ['Letter', 'Mark', 'Number', 'Punctuation', 'Symbol', 'Separator', 'Other']
+UNICODE_CATEGORIES_COMBINED = {
+    'Letter': ['Lu', 'Ll', 'Lt', 'Lm', 'Lo'],
+    'Mark': ['Mn', 'Mc', 'Me'],
+    'Number': ['Nd', 'Nl', 'No'],
+    'Punctuation': ['Pc', 'Pd', 'Ps', 'Pe', 'Pi', 'Pf', 'Po'],
+    'Symbol': ['Sm', 'Sc', 'Sk', 'So'],
+    'Separator': ['Zs', 'Zl', 'Zp'],
+    'Other': ['Cc', 'Cf', 'Cs', 'Co', 'Cn'],
+}
+def dec2hex(cls, n, uni = 1):
+    hex = "%X" % n
+    if uni == 1:
+    	while len(hex) <= 3:
+    		hex = '0' + str(hex)
+    return hex
+def readUnicode(cls, unicodeInteger):
+    if type(unicodeInteger) is int:
+        return dec2hex(unicodeInteger)
+    else:
+        return str(unicodeInteger)
+def reverseDict(d):
+	"""
+	Reverse a dictionary. This only works right when the dictionary has unique keys and unique values.
+	"""
+	newDict = {}
+	keys = d.keys()
+	keys.sort()
+	for k in keys:
+		v = d[k]
+		if v not in newDict.keys():
+			newDict[v] = k
+	return newDict
+
+
+# ----------------------------------------------------------------------------------
+# Glyph Search
+
+class SelectGlyphSearch(BaseWindowController):
+    """
+    Search for glyphs using a variety of parameters:
+    
+    - Glyph Name:
+        Exact match for glyph name.
+    
+    - Base Name:
+        Exact match for the basename of the glyph (anything before the first period in a glyph name)
+    
+    - Suffix
+        Exact match for the suffix of the glyph name (anything after the first period).
+
+    - Unicode value
+        Find exact match for hexadecimal unicode value.
+    
+    - Unicode category
+        The unicode category groups and individual categories are listed in a dropdown. 
+        You might have to hit enter/return in the dropdown to select an item.
+        These categories are inclusive, so they will match base glyphs and alternates ("number" will match 'zero' and also 'zero.sups'). 
+
+    You can use the following wildcards:
+        *	matches everything
+        ?	matches any single character
+        [seq]	matches any character in seq
+        [!seq]	matches any character not in seq
+
+    Then you can manually select glyphs (by default, all search results are selected)
+
+    You can perform the following selection manipulations in the current font or in all open fonts:
+
+    - Replace selection with results
+    - Add results to selection
+    - Subtract results from selection
+    - Intersect results and seletion (select glyphs common to both) 
+    - Print the glyph list to the output window.
+
+    """
+    
+    def __init__(self):
+        """
+        Initialize the dialog.
+        """        
+        title = "Glyph Select from Current Font"    
+        self.w = Window((400, 500), title, minSize=(400, 500))
+        # get the current font and selection
+        f = self.getFont()
+        if not f:
+            print "Open a font."
+            return
+        self.setExistingSelection(f.selection[:])
+        # make tabs
+        tabList = ['Name', 'Basename', 'Suffix', 'Unicode', 'Category']
+        self.w.tabs = Tabs((10, 10, -10, -10), tabList, sizeStyle="small")
+        for i, tabName in enumerate(tabList):
+            # search box
+            if i == 0 or i == 1 or i == 2 or i == 3:
+                self.w.tabs[i].searchBox = SearchBox((10, 10, -50, 22), callback=self.searchBoxCallback, placeholder=tabName)
+            else:
+                self.w.tabs[i].searchBox = ComboBox((10, 10, -50, 22), self.getUnicodeCategoriesList(), callback=self.searchBoxCallback)
+            # refresh button
+            self.w.tabs[i].refresh = Button((-40, 10, 30, 22), unichr(8634), callback=self.refreshCallback)
+            # glyph list
+            self.w.tabs[i].glyphList = List((10, 40, -10, -115), [])
+            #self.w.tabs[i].spinner = ProgressSpinner((-33, 5, 32, 32), displayWhenStopped=False, sizeStyle="small")
+        # buttons
+        y = -120
+        self.w.sourceName = TextBox((20, y, -10, 22), 'Source:', sizeStyle="small")
+        y += 22
+        self.w.replaceSelection = Button((20, y, 170, 22), 'Replace Selection', sizeStyle="small", callback=self.replaceSelection)       
+        self.w.addToSelection = Button((210, y, 170, 22), 'Add To Selection', sizeStyle="small", callback=self.addToSelection)
+        y += 27
+        self.w.intersectWithSelection = Button((20, y, 170, 22), 'Intersect with Selection', sizeStyle="small", callback=self.intersectWithSelection)
+        self.w.subtractFromSelection = Button((210, y, 170, 22), 'Subtract from Selection', sizeStyle="small", callback=self.subtractFromSelection)
+        y += 30
+        self.w.applySelectionText = TextBox((20, y, 120, 22), 'Apply selection:', sizeStyle="small")
+        self.w.applySelectionRadio = RadioGroup((120, y-3, 210, 22),
+                                        ["Current Font", "All Open Fonts"],
+                                        isVertical=False, sizeStyle="small")
+        self.w.applySelectionRadio.set(0)
+        #10169
+        self.w.printButton = Button((-50, y-3, 30, 22), unichr(9998), callback=self.printCallback, sizeStyle="small")
+        self.setUpBaseWindowBehavior()
+        self.w.open()
+
+    def windowCloseCallback(self, sender):
+        BaseWindowController.windowCloseCallback(self, sender)
+    
+    ###################
+    ## GETS AND SETS ##
+    ###################
+        
+    def getFont(self):
+        """
+        Returns the current font.
+        The other option was to do this with an observer, but that just seemed...complicated.
+        """
+        f = CurrentFont()
+        return f
+
+    def setFontSource(self, f):
+        if f is not None:
+            self.w.sourceName.set('Source: ' + self.getFontName(f))
+        else:
+            self.w.sourceName.set('Source:')
+    
+    def getFontName(self, f=None):
+        if f is None:
+            f = self.getFont()
+        return ' '.join([f.info.familyName, f.info.styleName])
+    
+    def getAllFonts(self):
+        return AllFonts()
+    
+    def getGlyphOrder(self):
+        return self.getFont().glyphOrder
+    
+    def getUnicodeData(self):
+        return self.getFont().naked().unicodeData
+    
+    def getFontAndGlyphOrderAndUnicodeData(self):
+        """
+        Convenience function.
+        """
+        f = self.getFont()
+        glyphOrder = f.glyphOrder
+        unicodeData = f.naked().unicodeData
+        return f, glyphOrder, unicodeData
+
+    def getExistingSelection(self):
+        return self.selection
+        
+    def setExistingSelection(self, selection):
+        self.selection = selection
+        
+    def getNewSelection(self):
+        """
+        Return the current selection within glyphList.
+        """
+        i = self.w.tabs.get()
+        tabAll = self.w.tabs[i].glyphList.get()
+        tabSelectedIndexes = self.w.tabs[i].glyphList.getSelection()
+        newSelection = []
+        for selectedIndex in tabSelectedIndexes:
+            newSelection.append(tabAll[selectedIndex])
+        return newSelection
+
+    ###############
+    ## CALLBACKS ##
+    ###############
+    
+    def searchBoxCallback(self, sender):
+        """
+        """
+        searchTerm = sender.get()
+        self.doSearch(searchTerm)
+
+    def refreshCallback(self, sender):
+        """
+        Refresh the search with the current font.
+        """
+        i = self.w.tabs.get()
+        self.doSearch(self.w.tabs[i].searchBox.get())
+    
+    def printCallback(self, sender):
+        print self.getNewSelection()
+    
+    ###############
+    ## DO SEARCH ##
+    ###############
+    
+    def doSearchInGlyphName(self, searchTerm, searchIn):
+        searchResults = []
+        for gname in searchIn:
+            if fnmatch(gname, searchTerm):
+                searchResults.append(gname)
+        return searchResults
+        
+    def doSearchInBaseName(self, searchTerm, searchIn):
+        searchResults = []
+        for gname in searchIn:
+            baseName, suffix = splitName(gname)
+            if fnmatch(baseName, searchTerm):
+                searchResults.append(gname)
+        return searchResults
+
+    def doSearchInSuffix(self, searchTerm, searchIn):
+        searchResults = []
+        for gname in searchIn:
+            baseName, suffix = splitName(gname)
+            if fnmatch(suffix, searchTerm):
+                searchResults.append(gname)
+        return searchResults
+
+    def getUnicodeCategoriesList(self):
+        categoryList = []
+        for metaCategoryName in UNICODE_CATEGORY_ORDER_COMBINED:
+            categoryList.append(metaCategoryName)
+        categoryList.append('')
+        for catshort in UNICODE_CATEGORY_ORDER:
+            categoryList.append(UNICODE_CATEGORIES[catshort])
+        return categoryList
+
+    def doSearchInUnicode(self, searchTerm, searchIn, unicodeData=None):
+        searchResults = []
+        if unicodeData is None:
+            unicodeData = self.getUnicodeData()
+        for gname in searchIn:
+            dec = unicodeData.unicodeForGlyphName(gname)
+            hex = readUnicode(dec)
+            if fnmatch(hex, searchTerm) and not gname in searchResults:
+                searchResults.append(gname)
+        searchResults.sort()
+        return searchResults
+        
+    def doSearchInUnicodeCategory(self, searchTerm, searchIn, unicodeData=None):
+        searchResults = []
+        if unicodeData is None:
+            unicodeData = self.getUnicodeData()
+        reverseUnicodeCategories = reverseDict(UNICODE_CATEGORIES)
+        try:
+            searchTerms = [reverseUnicodeCategories[searchTerm]]
+        except:
+            searchTerms = UNICODE_CATEGORIES_COMBINED[searchTerm]
+        for gname in searchIn:
+            if unicodeData.categoryForGlyphName(gname) in searchTerms:
+                searchResults.append(gname)
+        return searchResults
+
+    def doSearch(self, searchTerm):
+        f, glyphOrder, unicodeData = self.getFontAndGlyphOrderAndUnicodeData()
+        self.setFontSource(f)        
+        i = self.w.tabs.get()
+        if not searchTerm:
+            self.setFontSource(None)
+        searchResults = []
+        #start the spinner
+        #self.w.tabs[i].spinner.start()
+        if i == 0:   # glyph name
+            searchResults = self.doSearchInGlyphName(searchTerm, glyphOrder)
+        elif i == 1:  # base name
+            searchResults = self.doSearchInBaseName(searchTerm, glyphOrder)
+        elif i == 2:    # suffix
+            searchResults = self.doSearchInSuffix(searchTerm, glyphOrder)
+        elif i == 3:    # unicode value
+            searchResults = self.doSearchInUnicode(searchTerm, glyphOrder, unicodeData)
+        elif i == 4:    # unicode category
+            searchResults = self.doSearchInUnicodeCategory(searchTerm, glyphOrder, unicodeData)
+        else:
+            print 'tab error', i
+        # set the search results
+        self.w.tabs[i].glyphList.set(searchResults)
+        # select all by default
+        self.w.tabs[i].glyphList.setSelection(range(0, len(searchResults)))
+        # stop the spinner
+        #self.w.tabs[i].spinner.stop()
+
+    ############################
+    ##  MANIPULATE SELECTION  ##
+    ############################
+    
+
+    
+    def selectInFont(self):
+        # figure out whether we are selecting in current font, or all fonts
+        applySelectionIndex = self.w.applySelectionRadio.get()
+        if applySelectionIndex == 1:
+            fonts = self.getAllFonts()
+        elif applySelectionIndex == 0:
+            fonts = [self.getFont()]
+        # apply selection
+        for f in fonts:
+            fontSet = set(f.keys())
+            selectionSet = set(self.getExistingSelection())
+            resultSet = fontSet.intersection(selectionSet)
+            f.selection = list(resultSet)
+            f.update()
+    
+    def addToSelection(self, sender):
+        existingSet = set(self.getExistingSelection())
+        newSet = set(self.getNewSelection())
+        bothSet = existingSet | newSet
+        self.setExistingSelection(list(bothSet))
+        self.selectInFont()
+ 
+    def intersectWithSelection(self, sender):
+        existingSet = set(self.getExistingSelection())
+        newSet = set(self.getNewSelection())
+        bothSet = existingSet & newSet
+        self.setExistingSelection(list(bothSet))
+        self.selectInFont()
+ 
+    def subtractFromSelection(self, sender):
+        existingSet = set(self.getExistingSelection())
+        newSet = set(self.getNewSelection())
+        diffSet = existingSet - newSet
+        self.setExistingSelection(list(diffSet))
+        self.selectInFont()
+
+    def replaceSelection(self, sender):
+        self.setExistingSelection(self.getNewSelection())
+        self.selectInFont()
+
+    def clearSelection(self, sender):
+        self.setExistingSelection([])
+        self.selectInFont()
+
+if __name__ is "__main__":
+    OpenWindow(SelectGlyphSearch)
---
+++ OverlayUFOs/Overlay UFOs.roboFontExt/lib/OverlayUFOs.py
@@ -0,0 +1,332 @@
+# OVERLAY UFOS
+
+"""
+In the current glyph window, this will presenteview the same glyph from a separate UFO or set of UFOs.
+
+This does NOT import the UFO into a background layer. Instead, it renders a outline directly from the UFO into the glyph window view.
+
+- There is no need to import duplicate data into a background layer.
+
+- The source outline is always live; when changes are made to the source, they will automatically appear in the current without re-importing.
+
+- The source font does not need to be opened with a UI.
+
+DIALOG:
+    
+A floating dialog is present to let you open and select source fonts, fill, stroke, color.
+
+Source Fonts: The default source font list is AllFonts(). The refresh button will return this list to AllFonts().
+
+Adding Fonts: You can manually add fonts by selecting a UFO file. The UFO file will open without an interface.
+
+Removing Fonts: There are buttons for removing selected fonts and for clearing the source font list. 
+
+BUGS/IMPROVEMENTS:
+
+Known Issue: The source font is drawn on top of the current font, instead of behind it. So, it is good to select a color with a low opacity.
+
+Known Bug: If the glyph window for both source and current fonts are open, it is possible to select and inadvertently edit the source outline in the current window. I don't know how to solve this.
+
+Improvement?: Add options to scale the source font.
+
+Improvement?: Set different colors, fill settings for each font?
+
+
+"""
+
+from mojo.drawingTools import *
+from mojo.events import addObserver, removeObserver
+from fontTools.pens.transformPen import TransformPen
+from vanilla import *
+from AppKit import *
+from mojo.UI import UpdateCurrentGlyphView
+from defconAppKit.windows.baseWindow import BaseWindowController
+import os
+
+
+from mojo.extensions import getExtensionDefault, setExtensionDefault, getExtensionDefaultColor, setExtensionDefaultColor
+
+class MojoDrawingToolsPen(object):
+    def __init__(self, g, f):
+        self.g = g
+        self.f = f
+        newPath()
+    def moveTo(self, pt):
+        x, y = pt
+        moveTo((x, y))
+    def lineTo(self, pt):
+        x, y = pt
+        lineTo((x, y))
+    def curveTo(self, pt1, pt2, pt3):
+        h1x, h1y = pt1
+        h2x, h2y = pt2
+        x, y = pt3
+        curveTo((h1x, h1y), (h2x, h2y), (x, y))
+    def closePath(self):
+        closePath()
+    def endPath(self):
+        closePath()
+    def draw(self):
+        drawPath()
+    def addComponent(self, baseName, transformation):
+        glyph = self.f[baseName]
+        tPen = TransformPen(self, transformation)
+        glyph.draw(tPen)
+
+class ViewSourceFonts(object):
+    def __init__(self):
+        ## add an observer for the draw event
+        self.addObserver()
+        self.cacheCopy = None
+
+    def addObserver(self):
+        addObserver(self, "drawSourceFonts", "draw")
+
+    def removeObserver(self):
+        removeObserver(self, "draw")
+
+    def getSources(self):
+        sources = AllFonts()
+        if sources:
+            sources.pop(sources.index(CurrentFont()))
+            return sources
+        else:
+            return []
+    
+    def getAlignment(self):
+        return 'left'
+    
+    def drawSourceFonts(self, info):
+        glyph = info['glyph']
+        ## draw something the glyph view
+        
+        self.setStroke()
+        self.setFill()
+                
+        for source in self.getSources():
+            if source != CurrentFont():
+                if source.has_key(glyph.name):
+                    sourceGlyph = source[glyph.name]
+                    sourceFont = sourceGlyph.getParent()
+                    """
+                    if self.cacheCopy and self.cacheCopy.name == sourceGlyph.name:
+                        sourceGlyphCopy = self.cacheCopy
+                    else:
+                        sourceGlyphCopy = sourceGlyph.copy()
+                        self.cacheCopy = sourceGlyphCopy
+                    """
+                    sourceGlyphCopy = sourceGlyph.copy()
+                    
+                    align = self.getAlignment()
+                    if align == 'left':
+                        pass
+                    elif align == 'center':
+                        destCenter = int(glyph.width/2)
+                        sourceCenter = int(sourceGlyphCopy.width/2)
+                        widthDiff = destCenter-sourceCenter
+                        sourceGlyphCopy.move((widthDiff, 0))
+                    elif align == 'right':
+                        widthDiff = glyph.width-sourceGlyphCopy.width
+                        sourceGlyphCopy.move((widthDiff, 0))                        	
+
+                    mPen = MojoDrawingToolsPen(sourceGlyphCopy, sourceFont)
+                                        
+                    sourceGlyphCopy.draw(mPen)
+                    mPen.draw()
+                    self.updateView()
+
+    def setStroke(self, value=.5):
+        strokeWidth(value)
+    
+    def setFill(self, rgba=(.2, 0, .2, .2)):
+        r, g, b, a = rgba
+        fill(r, g, b, a)
+    
+    def getColor(self):
+        return NSColor.colorWithCalibratedRed_green_blue_alpha_(.5, 0, .5, .2)
+
+defaultKey = "com.fontbureau.viewSourceFonts"
+
+class ViewSourceFontsDialog(BaseWindowController, ViewSourceFonts):
+    def __init__(self):
+        ViewSourceFonts.__init__(self)
+        
+        self.sources = []
+        self.w = FloatingWindow((400, 200), "Overlay UFOs", minSize=(400, 200))
+
+        x = 10
+        y = 10
+
+        self.w.view = CheckBox((x, y, -10, -10), "Show Overlays", 
+                                             callback=self.viewCallback, 
+                                             value=True)
+
+
+        self.w.defaultPaths = Button((-230, y, 30, 22), unichr(8634), callback=self.defaultPathsCallback)
+
+        self.w.addPath = Button((-200, y, 30, 22), '+', callback=self.addPathCallback)
+        self.w.removePath = Button((-170, y, 30, 22), '-', callback=self.removePathCallback)
+        self.w.clearPaths = Button((-140, y, 30, 22), 'x', callback=self.clearPathsCallback)
+
+        self.w.fontList = List((10, 40, -110, -10), self.getDefaultSources(), 
+                               drawFocusRing=True, 
+                               enableDelete=True, 
+                               otherApplicationDropSettings=dict(type=NSFilenamesPboardType, operation=NSDragOperationCopy, callback=self.dropCallback) 
+                               ) 
+
+                
+        y = 40
+        
+        self.w.fill = CheckBox((-100, y, 60, 22), "Fill", 
+                               #value=getExtensionDefault("%s.%s" %(defaultKey, "fill"), True),
+                               value = True,
+                               callback=self.fillCallback)
+        y += 30
+        self.w.stroke = CheckBox((-100, y, 60, 22), "Stroke", 
+                               #value=getExtensionDefault("%s.%s" %(defaultKey, "stroke"), False),
+                               value = False, 
+                               callback=self.strokeCallback)
+        
+        y += 30
+        color = NSColor.colorWithCalibratedRed_green_blue_alpha_(.5, 0, .5, .1)
+
+        self.w.color = ColorWell((-100, y, 60, 22), 
+                                 color=color,
+                                 callback=self.colorCallback)
+        
+        
+        y += 30
+        self.w.alignText = TextBox((-100, y, 90, 50), 'Alignment')
+        y += 10
+
+        self.w.align = RadioGroup((-100, y, 60, 50), ['L', 'C', 'R'], isVertical=False, callback=self.alignCallback)
+        self.w.align.set(0)
+        
+        self.setUpBaseWindowBehavior()
+        
+        self.w.open()
+        self.updateView()
+
+
+    def dropCallback(self, sender, dropInfo): 
+        acceptedFonts = [".ufo", ".ttf", ".otf"] 
+        isProposal = dropInfo["isProposal"] 
+        paths = dropInfo["data"]
+        paths = [path for path in paths if os.path.splitext(path)[-1].lower() in acceptedFonts] 
+        fonts = []
+        for path in paths:
+            if path:
+                font = RFont(path, showUI=False)
+                fonts.append(font)
+        fonts = [font for font in fonts if font not in self.w.fontList] 
+        if not fonts: 
+            return False 
+        if not isProposal and fonts:
+            self.extendSources(fonts)
+        return True 
+
+    def viewCallback(self, sender):
+        if sender.get():
+            self.addObserver()
+        else:
+            self.removeObserver()
+        self.updateView()
+    
+    def getSources(self):
+        return self.w.fontList.get()
+        
+    def setSources(self, fontList):
+        self.w.fontList.set(fontList)
+        self.updateView()
+    
+    def extendSources(self, fontList):
+        self.w.fontList.extend(fontList)
+        self.updateView()
+    
+    def getDefaultSources(self):
+        sources = AllFonts()
+        return sources
+    
+    def addPathCallback(self, sender):
+        f = OpenFont(None, showUI=False)
+        sources = self.getSources()
+        if type(f) is not list:
+            f = [f]
+        if f:
+            self.extendSources(f)
+            self.updateView()
+        
+    def removePathCallback(self, sender):
+        selection = self.w.fontList.getSelection()
+        selection.reverse()
+        for item in selection:
+            self.w.fontList.remove(self.w.fontList[item])
+        self.updateView()
+        
+    def clearPathsCallback(self, sender):
+        self.setSources([])
+        self.updateView()
+        
+    def defaultPathsCallback(self, sender):
+        self.setSources(self.getDefaultSources())
+        self.updateView()
+        
+    def colorCallback(self, sender):
+        setExtensionDefaultColor("%s.%s" %(defaultKey, "color"), sender.get())
+        self.updateView()
+    
+    def fillCallback(self, sender):
+        setExtensionDefault("%s.%s" %(defaultKey, "fill"), sender.get())
+        self.setFill()
+        self.updateView()
+    
+    def strokeCallback(self, sender):
+        setExtensionDefault("%s.%s" %(defaultKey, "stroke"), sender.get())
+        self.setStroke()
+        self.updateView()
+        
+    def alignCallback(self, sender):
+        self.updateView()
+    
+    def getAlignment(self):
+        index = self.w.align.get()
+        if index == 0:
+            return 'left'
+        elif index == 1:
+            return 'center'
+        elif index == 2:
+            return 'right'
+
+    def updateView(self, sender=None):
+        UpdateCurrentGlyphView()
+
+    def windowCloseCallback(self, sender):
+        self.removeObserver()
+        self.updateView()
+        BaseWindowController.windowCloseCallback(self, sender)
+        
+    def getColor(self):
+        c = self.w.color.get()
+        return c.getRed_green_blue_alpha_(None, None, None, None)
+
+    def setStroke(self):
+        if self.w.stroke.get():
+            strokeWidth(.5)
+            r,g,b,a = self.getColor()
+            a += .3
+            if a > 1:
+                a = 1
+            stroke(r, g, b, a)
+        else:
+            strokeWidth(0)
+            stroke(1, 1, 1, 0)
+    
+    def setFill(self):
+        if self.w.fill.get():
+            r,g,b,a = self.getColor()
+            fill(r,g,b,a)
+        else:
+            fill(1, 1, 1, 0)
+    
+        
+ViewSourceFontsDialog()
