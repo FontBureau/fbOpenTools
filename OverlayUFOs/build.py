40c0d0e2ffbb5aa591b9ae6593759bbae95f6a06
Roberto Arista <arista.rob@gmail.com>
2022-02-10 14:10:16 +0100

    black formatting + adapting to overlayUFOs

--- OverlayUFOs/build.py
+++ OverlayUFOs/build.py
@@ -1,81 +1,74 @@
-'''build RoboFont Extension'''
+"""build RoboFont Extension"""
 
 import os
-from AppKit import NSCommandKeyMask, NSAlternateKeyMask, NSShiftKeyMask
+
+from AppKit import NSAlternateKeyMask, NSCommandKeyMask, NSShiftKeyMask
 from mojo.extensions import ExtensionBundle
 
 # get current folder
 basePath = os.path.dirname(__file__)
 
 # source folder for all extension files
-sourcePath = os.path.join(basePath, 'source')
+sourcePath = os.path.join(basePath, "source")
 
 # folder with python files
-libPath = os.path.join(sourcePath, 'code')
+libPath = os.path.join(sourcePath, "code")
 
 # folder with html files
-htmlPath = os.path.join(sourcePath, 'documentation')
+htmlPath = os.path.join(sourcePath, "documentation")
 
 # folder with resources (icons etc)
-resourcesPath = os.path.join(sourcePath, 'resources')
+resourcesPath = os.path.join(sourcePath, "resources")
 
 # load license text from file
 # see choosealicense.com for more open-source licenses
-licensePath = os.path.join(basePath, 'license.txt')
-
-# required extensions
-requirementsPath = os.path.join(basePath, 'requirements.txt')
+licensePath = os.path.join(basePath, "license.txt")
 
 # name of the compiled extension file
-extensionFile = 'myExtension.roboFontExt'
+extensionFile = "OverlayUFOs.roboFontExt"
 
 # path of the compiled extension
-buildPath = os.path.join(basePath, 'build')
+buildPath = os.path.join(basePath, "build")
 extensionPath = os.path.join(buildPath, extensionFile)
 
 # initiate the extension builder
 B = ExtensionBundle()
 
 # name of the extension
-B.name = "myExtension"
+B.name = "OverlayUFOs"
 
 # name of the developer
-B.developer = 'RoboDocs'
+B.developer = "Cyrus Highsmith"
 
 # URL of the developer
-B.developerURL = 'http://github.com/roboDocs'
+B.developerURL = "http://github.com/roboDocs"
 
 # extension icon (file path or NSImage)
-imagePath = os.path.join(resourcesPath, 'icon.png')
+imagePath = os.path.join(resourcesPath, "icon.png")
 B.icon = imagePath
 
 # version of the extension
-B.version = '0.2.6'
+B.version = "2.0.0"
 
 # should the extension be launched at start-up?
-B.launchAtStartUp = True
+B.launchAtStartUp = False
 
 # script to be executed when RF starts
-B.mainScript = 'hello.py'
+B.mainScript = "OverlayUFOs.py"
 
 # does the extension contain html help files?
 B.html = True
 
 # minimum RoboFont version required for this extension
-B.requiresVersionMajor = '4'
-B.requiresVersionMinor = '0'
+B.requiresVersionMajor = "4"
+B.requiresVersionMinor = "2"
 
 # scripts which should appear in Extensions menu
 B.addToMenu = [
     {
-        'path':          'doSomething.py',
-        'preferredName': 'do something',
-        'shortKey':      (NSCommandKeyMask | NSShiftKeyMask, 'b'),
-    },
-    {
-        'path':          'doSomethingElse.py',
-        'preferredName': 'do something else',
-        'shortKey':      (NSAlternateKeyMask, 'o'),
+        "path": "OverlayUFOs.py",
+        "preferredName": "OverlayUFOs",
+        # "shortKey": (NSCommandKeyMask | NSShiftKeyMask, "b"),
     }
 ]
 
@@ -83,17 +76,10 @@ B.addToMenu = [
 with open(licensePath) as license:
     B.license = license.read()
 
-# required extensions
-with open(requirementsPath) as requirements:
-    B.requirements = requirements.read()
-
-# expiration date for trial extensions
-B.expireDate = '2020-12-31'
-
 # compile and save the extension bundle
-print('building extension...', end=' ')
-B.save(extensionPath, libPath=libPath, htmlPath=htmlPath, resourcesPath=resourcesPath, pycOnly=pycOnly)
-print('done!')
+print("building extension...", end=" ")
+B.save(extensionPath, libPath=libPath, htmlPath=htmlPath, resourcesPath=resourcesPath)
+print("done!")
 
 # check for problems in the compiled extension
 print()
